f0ae699ecd2c1d5c8abe855a66227034f508efe0
Abishekponmudi <shekabi827@gmail.com>
2025-03-20 11:07:27 +0530

    Minor UPdate  - Testing serve issue foxed

--- TASK 1 - backend/main.py
+++ TASK 1 - backend/main.py
@@ -1,11 +1,15 @@
+#written by Abishek 7376221EI104  - AFFORDMED TEST
+
+"""QUESTIONS :
+    Develope average calculator HTTP microservoce
+    """
+
+##code for test cases 
+
 # from fastapi import FastAPI, Path, Query
 # import requests
 
 # app = FastAPI()
-
-# urls_random = "http://20.244.56.144/test/rand"
-# url_fibo = "http://20.244.56.144/test/fibo"
-# url_primes = "http://20.244.56.144/test/primes"
 
 # token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzQyNDQ2NDA5LCJpYXQiOjE3NDI0NDYxMDksImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjIzN2E2NTViLTQyOWQtNDE4My04MWQ1LTU3MGUzYWU2NGY2MSIsInN1YiI6ImFiaXNoZWsuZWkyMkBiaXRzYXRoeS5hYy5pbiJ9LCJjb21wYW55TmFtZSI6ImdvTWFydCIsImNsaWVudElEIjoiMjM3YTY1NWItNDI5ZC00MTgzLTgxZDUtNTcwZTNhZTY0ZjYxIiwiY2xpZW50U2VjcmV0Ijoicmtnd2R4Y1pFaHl1WWZIWSIsIm93bmVyTmFtZSI6IkFiaXNoZWsiLCJvd25lckVtYWlsIjoiYWJpc2hlay5laTIyQGJpdHNhdGh5LmFjLmluIiwicm9sbE5vIjoiNzM3NjIyMUVJMTA0In0.82zl8W9V6KX1L4qmfxW_las8YiCO0ley_mqlfMSSBKY"
 
@@ -13,20 +17,32 @@
 #     "Authorization": f"Bearer {token}"
 # }
 
+##Random number API 
+
+# urls_random = "http://20.244.56.144/test/rand"
+
 # response1 = requests.get(urls_random, headers=headers)
-# print("Status Code:", response1.status_code)
-# print("Response Body:", response1.json())
+# print("status code:", response1.status_code)
+# print("response body:", response1.json())
+
+## Fibinocci number API
+
+# url_fibo = "http://20.244.56.144/test/fibo"
 
 # response2 = requests.get(url_fibo, headers=headers)
-# print("Status Code:", response2.status_code)
-# print("Response Body:", response2.json())
+# print("status code:", response2.status_code)
+# print("response body:", response2.json())
+
+
+##Prime number api
+# url_primes = "http://20.244.56.144/test/primes"
 
 # response3 = requests.get(url_primes, headers=headers)
-# print("Status Code:", response3.status_code)
-# print("Response Body:", response3.json())
+# print("status code:", response3.status_code)
+# print("response body:", response3.json())
 
 
-# CODE FOR TASK TESTASE 1
+## CODE FOR TASK TESTASE 1
 
 # from fastapi import FastAPI, HTTPException
 # from pydantic import BaseModel
@@ -49,6 +65,7 @@
 #     return response
 
 
+## Code for Test Case - 1 
 
 from fastapi import FastAPI
 
@@ -58,15 +75,14 @@ window_curr_state = [2,4,6,8]
 numbers =[2,4,6,8]
 avg = 0
 
-class State:
-  
-    def update_state(new_number: int):
+
+def update_state(new_number: int = None):
         window_prev_state = window_curr_state[:]
         window_curr_state.append(new_number)
         numbers = window_prev_state +window_curr_state
         avg = sum(numbers) / len(numbers) if numbers else 0
-
-    def get_response(self):
+        
+def get_response():
         return {
             "windowPrevState": window_prev_state,
             "windowCurrState": window_curr_state,
@@ -74,10 +90,8 @@ class State:
             "avg": round(avg, 2)
         }
 
-state = State()
-
 @app.get("/number/e")
 def get_state(number: int = None):
     if number is not None:
-        state.update_state(number)
-    return state.get_response()
+        update_state(number)
+    return get_response()
--- TASK 2 - backend/main.py
+++ TASK 2 - backend/main.py
@@ -1,17 +1,32 @@
+#written by Abishek 7376221EI104  - AFFORDMED TEST
+
+"""QUESTIONS :
+    Develope a Social Media Analytics HTTP Microservice
+    """
+
+#CODE FOR TEST SOCIAL MEDIA SERVER API'S
+
 from fastapi import FastAPI, Path, Query
 import requests
 
 app = FastAPI()
 
-urls_random = "http://20.244.56.144/test/users"
+# urls = "http://20.244.56.144/test/users"
+urls = "http://20.244.56.144/test/users/1/posts"
+# urls = "http://20.244.56.144/test/posts/150/comments"
 
 
-token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzQyNDQ2NDA5LCJpYXQiOjE3NDI0NDYxMDksImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjIzN2E2NTViLTQyOWQtNDE4My04MWQ1LTU3MGUzYWU2NGY2MSIsInN1YiI6ImFiaXNoZWsuZWkyMkBiaXRzYXRoeS5hYy5pbiJ9LCJjb21wYW55TmFtZSI6ImdvTWFydCIsImNsaWVudElEIjoiMjM3YTY1NWItNDI5ZC00MTgzLTgxZDUtNTcwZTNhZTY0ZjYxIiwiY2xpZW50U2VjcmV0Ijoicmtnd2R4Y1pFaHl1WWZIWSIsIm93bmVyTmFtZSI6IkFiaXNoZWsiLCJvd25lckVtYWlsIjoiYWJpc2hlay5laTIyQGJpdHNhdGh5LmFjLmluIiwicm9sbE5vIjoiNzM3NjIyMUVJMTA0In0.82zl8W9V6KX1L4qmfxW_las8YiCO0ley_mqlfMSSBKY"
+token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzQyNDQ4NjYwLCJpYXQiOjE3NDI0NDgzNjAsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjIzN2E2NTViLTQyOWQtNDE4My04MWQ1LTU3MGUzYWU2NGY2MSIsInN1YiI6ImFiaXNoZWsuZWkyMkBiaXRzYXRoeS5hYy5pbiJ9LCJjb21wYW55TmFtZSI6ImdvTWFydCIsImNsaWVudElEIjoiMjM3YTY1NWItNDI5ZC00MTgzLTgxZDUtNTcwZTNhZTY0ZjYxIiwiY2xpZW50U2VjcmV0Ijoicmtnd2R4Y1pFaHl1WWZIWSIsIm93bmVyTmFtZSI6IkFiaXNoZWsiLCJvd25lckVtYWlsIjoiYWJpc2hlay5laTIyQGJpdHNhdGh5LmFjLmluIiwicm9sbE5vIjoiNzM3NjIyMUVJMTA0In0.uCGsE0fKrU6n_tzG7C4WJngQ_2OpCFB6rs8QLrbMpjg"
 
 headers = {
     "Authorization": f"Bearer {token}"
 }
 
-response1 = requests.get(urls_random, headers=headers)
-print("Status Code:", response1.status_code)
-print("Response Body:", response1.json())
+response1 = requests.get(urls, headers=headers)
+print("status code :", response1.status_code)
+print("response body : ", response1.json())
+
+
+# response = requests.get(urls,headers=headers)
+# print("status code :" ,response.status_code)
+# print("reponse body : ",response.json())
